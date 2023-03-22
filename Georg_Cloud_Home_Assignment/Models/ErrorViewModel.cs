4e0566e1cb985932d12741b25b120c7039841dd4
GeorgGrech <georg.grech.d57175@mcast.edu.mt>
2023-03-22 17:00:32 +0100

    init commit

---
+++ Georg_Cloud_Home_Assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Georg_Cloud_Home_Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
