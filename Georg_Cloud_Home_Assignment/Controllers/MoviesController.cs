92932a4e96541383b2fa2fb8f596ad002b8c7e9c
GeorgGrech <georg.grech.d57175@mcast.edu.mt>
2023-03-26 11:44:07 +0200

    Progress bar
    -Created progress bar in Movies/Create with JQuery
    -Changed File Upload as Async
    -Modified progressBar value with TempData

--- Georg_Cloud_Home_Assignment/Controllers/MoviesController.cs
+++ Georg_Cloud_Home_Assignment/Controllers/MoviesController.cs
@@ -2,6 +2,7 @@
 using Georg_Cloud_Home_Assignment.Models;
 using Google.Cloud.Storage.V1;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -13,8 +14,10 @@ namespace Georg_Cloud_Home_Assignment.Controllers
 {
     public class MoviesController : Controller
     {
-
         FirestoreMovieRepository fmr;
+
+        float uploadMax;
+
         public MoviesController(FirestoreMovieRepository _fmr)
         {
             fmr = _fmr;
@@ -23,12 +26,13 @@ namespace Georg_Cloud_Home_Assignment.Controllers
         [Authorize]
         public IActionResult Create()
         {
+            TempData["progress"] = null;
             return View();
         }
 
         [HttpPost]
         [Authorize]
-        public IActionResult Create(Movie m, IFormFile file)
+        public async Task<IActionResult> CreateAsync(Movie m, IFormFile file)
         {
             try
             {
@@ -40,7 +44,11 @@ namespace Georg_Cloud_Home_Assignment.Controllers
                     using var fileStream = file.OpenReadStream(); //reads the uploaded file from the server's memory
                     newFilename = Guid.NewGuid().ToString() + System.IO.Path.GetExtension(file.FileName);
 
-                    storage.UploadObject("georg_movie_app_bucket", newFilename, null, fileStream);
+
+                    uploadMax = fileStream.Length; //Set max to calculate progress bar percentage
+                    var progressReporter = new Progress<Google.Apis.Upload.IUploadProgress>(OnUploadProgress);
+                    await storage.UploadObjectAsync("georg_movie_app_bucket", newFilename, null, fileStream, progress: progressReporter);
+
                     m.Link = $"https://storage.googleapis.com/{"georg_movie_app_bucket"}/{newFilename}";
                 }
 
@@ -67,5 +75,43 @@ namespace Georg_Cloud_Home_Assignment.Controllers
             var list = t.Result;
             return View(list);
         }
+
+
+        void OnUploadProgress(Google.Apis.Upload.IUploadProgress progress)
+        {
+            switch (progress.Status)
+            {
+                case Google.Apis.Upload.UploadStatus.Starting:
+                    //ProgressBar.Minimum = 0;
+                    TempData["progressinfo"] = "Starting upload...";
+                    TempData["progress"] = 0;
+
+                    break;
+                case Google.Apis.Upload.UploadStatus.Completed:
+                    TempData["progressinfo"] = "Upload complete!";
+                    TempData["progress"] = 100;
+                    //System.Windows.MessageBox.Show("Upload completed"); //Set as TempData later
+
+                    break;
+                case Google.Apis.Upload.UploadStatus.Uploading:
+                    TempData["progressinfo"] = "Uploading file...";
+                    UpdateProgressBar(progress.BytesSent);
+
+                    break;
+                case Google.Apis.Upload.UploadStatus.Failed:
+                    TempData["progressinfo"] = "Upload failed.";
+                    /*System.Windows.MessageBox.Show("Upload failed"
+                                                   + Environment.NewLine
+                                                   + progress.Exception);*/
+                    break;
+            }
+        }
+
+        void UpdateProgressBar(long value)
+        {
+            TempData["progress"] = (value/uploadMax)*100;
+        }
+
+
     }
 }
